266b1ce99f66a8ea21710247d719d48e9e9e69a7
Kevin Le <Kevin.Le@fcma.com>
2023-08-14 17:09:38 -0400

    before output event emitter

--- APM-Start/src/app/product-list/product-list.component.ts
+++ APM-Start/src/app/product-list/product-list.component.ts
@@ -8,6 +8,7 @@ import { IProduct } from './product';
 })
 export class ProductListComponent implements OnInit {
 
+
   pageTitle: string = 'Product List'
   imageWidth: number = 50;
   imageMargin: number = 2;
