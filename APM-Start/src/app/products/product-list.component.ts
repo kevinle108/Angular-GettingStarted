9f2ecc651847e6b88dd9da89c9e03d93ea7eb273
Kevin Le <Kevin.Le@fcma.com>
2023-08-16 11:52:54 -0400

    done m10 - add httpclient to service

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
 
   pageTitle: string = 'Product List'
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errorMessage: string = '';
+  sub!: Subscription;
+
   private _listFilter = '';
 
   // inject the product service
@@ -24,8 +28,7 @@ export class ProductListComponent implements OnInit {
   set listFilter(value: string) {
     this._listFilter = value;
     console.log('In setter:', value);
-    this.filteredProducts = this.performFilter(value);
-    
+    this.filteredProducts = this.performFilter(value);    
   }
 
   filteredProducts: IProduct[] = [];
@@ -43,11 +46,21 @@ export class ProductListComponent implements OnInit {
 
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts()
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    }
+    )
     this.filteredProducts = this.products;
   }
 
-
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+  
   onRatingClicked(message: string): void {
     this.pageTitle = 'Product List: ' + message;
   }
